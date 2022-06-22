8bcf936fc5f47b6459aedc9ff8908e808220db5c
wertyq111 <wertyq222@163.com>
2022-06-22 15:35:07 +0800

    布置作业

---
+++ chapter11/src/com/homework/Homework01.java
@@ -0,0 +1,18 @@
+package com.homework;
+
+/**
+ * @author zhouxufeng
+ * @version 1.0
+ * 1. 创建一个 Color 枚举类
+ * 2. 有 RED, BLUE, BLACK, YELLOW, GREEN 五个枚举值/对象
+ * 3. Color 有三个属性 redValue, greenValue, blueValue
+ * 4. 创建构造方法, 参数包括这三个属性
+ * 5. 每个枚举值都要给这三个属性赋值, 三个属性堆应的值分别是:
+ * 6. red:255,0,0 blue:0,0,255 black:0,0,0 yellow:255,255,0 green:0,255,0
+ * 7. 定义接口, 里面有方法show, 要求 Color实现该接口
+ * 8. show方法中显示三属性的值
+ * 9. 将枚举对象在switch语句中匹配使用
+ */
+@SuppressWarnings({"all"})
+public class Homework01 {
+}
