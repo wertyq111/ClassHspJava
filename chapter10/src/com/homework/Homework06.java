5169f6ee945be6b2bcc0b3219f5150ec80dbfd2c
wertyq111 <wertyq222@163.com>
2022-06-22 15:30:14 +0800

    布置作业

---
+++ chapter10/src/com/homework/Homework06.java
@@ -0,0 +1,17 @@
+package com.homework;
+
+/**
+ * @author zhouxufeng
+ * @version 1.0
+ * 1. 有一个交通工具接口类 Vehicles, 有 work 方法
+ * 2. 有 Horse 类和 Boat 类分辨实现 Vehicles
+ * 3. 创建交通工具工厂类, 有两个方法分别获的交通工具 Horse 和 Boat
+ * 4. 有 Person 类 有 name 和 Vehicles 属性, 在构造器中为两个属性赋值
+ * 5. 实例化 Person 对象"唐僧", 要求一般情况下用 Horse 作为交通工具, 遇到大河时用 Boat 作为交通工具
+ */
+@SuppressWarnings({"all"})
+public class Homework06 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter10/src/com/homework/Homework07.java
@@ -0,0 +1,13 @@
+package com.homework;
+
+/**
+ * @author zhouxufeng
+ * @version 1.0
+ * 1. 有一个 Car 类, 有属性 temperature(温度)
+ * 2. 车内有 Air(空调)类, 有吹风功能flow
+ * 3. Air会监视车内温度, 如果温度超过40度则吹冷风, 如果温度低于0度则吹暖气, 如果在这期间则关掉空调
+ * 4. 实例化不同温度的 Car 对象, 调用空调flow 方法, 测试空调吹的风是否正确
+ */
+@SuppressWarnings({"all"})
+public class Homework07 {
+}
