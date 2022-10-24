5b1bfb2736af6041fdf483cee5693b3afac7826a
zhouxufeng <wertyq222@163.com>
2022-10-24 14:03:12 +0800

    chapter24 - 数据库操作

--- chapter24/src/com/javamysql/JavaMysql.java
+++ chapter24/src/com/javamysql/JavaMysql.java
@@ -14,7 +14,7 @@ public class JavaMysql {
     public static void main(String[] args) throws Exception {
         //加载类, 得到mysql 连接
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://1.117.211.237:3306/?serverTimezone=GMT%2B8", "wertyq111", "zhouXufeng123_");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://xxx.xxx.xxx.xxx:3306/?serverTimezone=GMT%2B8", "xxx", "xxx");
 
         //编写 sql 语句
         String sql = "update oa_sale.commodity where 1";
